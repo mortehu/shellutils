da88cf3e2a3ae85478a6360a9e07921a68769e47
Morten Hustveit <morten.hustveit@gmail.com>
2023-04-01 21:33:54 -0400

    Add some standard files

---
+++ README.md
@@ -0,0 +1,21 @@
+# Shellutils
+
+## Programs
+
+ * `v`: Find and edit a file with the specified name.
+
+## Installation
+
+To install the package, run:
+
+```bash
+pip install .
+```
+
+## Usage
+
+To use the `v` command, simply run:
+
+```bash
+v <filename>
+```
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shellutils",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "v = shellutils.v:main",
+        ],
+    },
+    author="Morten Hustveit",
+    author_email="morten.hustveit@gmail.com",
+    description="Miscellaneous shell utilities.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mortehu/shellutils",
+)
